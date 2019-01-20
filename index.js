d26b5c81da8384280e60b7ddc33cc43c77f287de
Agisilaos Kounelis <kounelisagis@gmail.com>
2019-01-20 13:02:02 +0200

    Add toggle instructions

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var win = false;
 var difficulty = +document.getElementById("difficulty-level").value;
 document.getElementById("user-input").maxLength = difficulty;
-let compInput=[];
+let compInput = [];
 
 // Generate array of 4 unique random numbers
 function generateCompInput() {
 	while (compInput.length < difficulty) {
 		const rand = Math.floor(Math.random() * 9);
-		if(compInput.indexOf(rand) === -1) {
+		if (compInput.indexOf(rand) === -1) {
 			compInput.push(rand);
 		}
 	}
@@ -15,27 +15,27 @@ function generateCompInput() {
 generateCompInput();
 
 //Get difficulty level
-document.getElementById("difficulty-level").onchange = function(){
+document.getElementById("difficulty-level").onchange = function () {
 	difficulty = +document.getElementById("difficulty-level").value;
 	generateCompInput();
 	document.getElementById("user-input").maxLength = difficulty;
 };
 
 function deadAndWounded(compInput, userInput) {
-	const userArray = (""+userInput).split("");
+	const userArray = ("" + userInput).split("");
 	let dead = 0;
 	let wounded = 0;
 
-	for(var i = 0; i < userArray.length; i++){
-        if (userArray[i] == compInput[i]) {
-            dead++;
-        } 
+	for (var i = 0; i < userArray.length; i++) {
+		if (userArray[i] == compInput[i]) {
+			dead++;
+		}
 		for (var j = 0; j < userArray.length; j++) {
 			if (i !== j && userArray[i] == compInput[j]) {
 				wounded++;
 			}
 		}
-	} 
+	}
 
 	if (dead === difficulty) {
 		win = true;
@@ -61,13 +61,13 @@ function guess() {
 	let userInput = document.getElementById("user-input").value;
 	let error = document.getElementById("form-errors");
 
-    if (userInput.length === 0 ) {
-        error.innerHTML = "Input cannot be empty"
+	if (userInput.length === 0) {
+		error.innerHTML = "Input cannot be empty"
 	} else if (isNaN(userInput)) {
-        error.innerHTML = "Input must be a number"
+		error.innerHTML = "Input must be a number"
 	} else if (userInput.length !== difficulty) {
-        error.innerHTML = `Input must be ${difficulty} characters`
-    } else if ([... new Set(userInput.split(""))].join("").length !== userInput.length) {
+		error.innerHTML = `Input must be ${difficulty} characters`
+	} else if ([...new Set(userInput.split(""))].join("").length !== userInput.length) {
 		document.getElementById("form-errors").innerHTML = "Cannot have duplicate values";
 		return;
 	} else {
@@ -77,13 +77,22 @@ function guess() {
 		} else {
 			addResultRow(userInput, deadAndWounded(compInput, userInput));
 		}
-    }
+	}
 }
 
-document.getElementById("user-input").onkeypress = function(event){
-    if (event.which == 13 || event.keyCode == 13) {
-        guess();
-        return false;
-    }
-    return true;
+document.getElementById("user-input").onkeypress = function (event) {
+	if (event.which == 13 || event.keyCode == 13) {
+		guess();
+		return false;
+	}
+	return true;
 };
+
+function toggleInstructions() {
+	var x = document.getElementById("instructions");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
